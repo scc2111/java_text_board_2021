dd3755635899eeb6711fa30394a2e8b5ec1f13ef
scc2111 <scc2111@naver.com>
2021-06-28 19:44:56 +0900

    추가기능 구현

--- src/com/sbs/exam/app/App.java
+++ src/com/sbs/exam/app/App.java
@@ -1,47 +1,154 @@
 package com.sbs.exam.app;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import com.sbs.exam.Util.Util;
 import com.sbs.exam.app.dto.Article;
 
-public class App {
 
+
+public class App {
 	public static void run() {
 		System.out.println("== 텍스트 게시판 시작 ==");
-		
+
 		Scanner sc = new java.util.Scanner(System.in);
-		
+
+		List<Article> articles = new ArrayList<>();
+		int articlesLastId = 0;
+
+		for (int i = 0; i < 10; i++) {
+			Article article = new Article();
+			article.id = articlesLastId + 1;
+			article.regDate = Util.getNowDateStr();
+			article.updateDate = Util.getNowDateStr();
+			article.title = "제목_" + article.id;
+			article.body = "내용_" + article.id;
+			articles.add(article);
+			articlesLastId++;
+		}
+
 		while (true) {
 			System.out.printf("명령어) ");
-			
+
 			String command = sc.nextLine().trim();
-			
-			if (command.equals("/usr/system/exit")) {
-				System.out.println("프로그램을 종료합니다.");
-				break;
-			}
-			
+
 			if (command.equals("/usr/article/write")) {
 				System.out.printf("제목: ");
 				String title = sc.nextLine().trim();
-				
-				
 				System.out.printf("내용: ");
 				String body = sc.nextLine().trim();
-				
+
 				Article article = new Article();
-				article.id = 1;
-				article.regDate = "2021-12-12";
-				article.updateDate = "2021-12-12";
+				article.id = articlesLastId + 1;
+				article.regDate = Util.getNowDateStr();
+				article.updateDate = Util.getNowDateStr();
 				article.title = title;
 				article.body = body;
-				System.out.println(article);
-				
+				articles.add(article);
+
+				articlesLastId++;
+
+				System.out.printf("%d번 게시물이 생성되었습니다.\n", article.id);
+			} else if (command.equals("/usr/article/list")) {
+				System.out.printf("번호 / 작성날자 / 제목\n");
+
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d / %s / %s\n", article.id, article.regDate, article.title);
+				}
+
+			} else if (command.equals("/usr/system/exit")) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			} else if (command.startsWith("/usr/article/detail")) {
+				String queryString = command.split("\\?", 2)[1];
+				String[] queryStringBits = queryString.split("&");
+
+				int id = 0;
+
+				for (String queryStringBit : queryStringBits) {
+					String[] queryStringBitBits = queryStringBit.split("=", 2);
+					String paramName = queryStringBitBits[0];
+					String paramValue = queryStringBitBits[1];
+
+					if (paramName.equals("id")) {
+						id = Integer.parseInt(paramValue);
+					}
+				}
+
+				if (id == 0) {
+					System.out.println("id를 입력해주세요.");
+					continue;
+				}
+
+				Article foundArticle = null;
+
+				for (Article article : articles) {
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("작성날짜 : %s\n", foundArticle.regDate);
+				System.out.printf("수정날짜 : %s\n", foundArticle.updateDate);
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+
+			} else if (command.startsWith("/usr/article/delete")) {
+				String queryString = command.split("\\?", 2)[1];
+				String[] queryStringBits = queryString.split("&");
+
+				int id = 0;
+
+				for (String queryStringBit : queryStringBits) {
+					String[] queryStringBitBits = queryStringBit.split("=", 2);
+					String paramName = queryStringBitBits[0];
+					String paramValue = queryStringBitBits[1];
+
+					if (paramName.equals("id")) {
+						id = Integer.parseInt(paramValue);
+					}
+				}
+
+				if (id == 0) {
+					System.out.println("id를 입력해주세요.");
+					continue;
+				}
+
+				Article foundArticle = null;
+
+				for (Article article : articles) {
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+
+				articles.remove(foundArticle);
+
+				System.out.printf("%d번 게시물을 삭제하였습니다.\n", id);
+
+			} else if (command.equals("/usr/system/exit")) {
+				System.out.println("프로그램을 종료합니다.");
 				break;
 			}
-			
-			
 		}
+
 		System.out.println("== 텍스트 게시판 끝 ==");
 	}
+
 }
--- src/com/sbs/exam/app/dto/Article.java
+++ src/com/sbs/exam/app/dto/Article.java
@@ -1,7 +1,7 @@
 package com.sbs.exam.app.dto;
 
-public class Article extends Object {
-	public int id;
+public class Article extends Object {   // extends Object 포함됨, 제외가능
+	public int id;     // 같은 파일이 아니라면 public (외부에 공개, 외부도 접근가능)
 	public String regDate;
 	public String updateDate;
 	public String title;
